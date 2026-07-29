ff61b78afb49c7779fa988385c010da8d701a016
Siddiqjonov <siddiqjonovsaidabror@gmail.com>
2026-07-29 21:03:28 +0500

    upload media file reuqest is fixed

--- NeoClinic/NeoClinic.Application/UserCases/MediaFiles/Upload/UploadMediaFileRequest.cs
+++ NeoClinic/NeoClinic.Application/UserCases/MediaFiles/Upload/UploadMediaFileRequest.cs
@@ -4,12 +4,13 @@ using NeoClinic.Domain.Enums;
 
 namespace NeoClinic.Application.UserCases.MediaFiles.Upload;
 
-public record UploadMediaFileRequest(
-    string? FileDescriptionUz,
-    string? FileDescriptionRu,
-    string? AltTextUz,
-    string? AltTextRu,
-    MediaType Type,
-    IFormFile File,
-    IFormFile? Thumbnail)
-    : IRequest<bool>;
+public class UploadMediaFileRequest : IRequest<bool>
+{
+    public string? FileDescriptionUz { get; set; }
+    public string? FileDescriptionRu { get; set; }
+    public string? AltTextUz { get; set; }
+    public string? AltTextRu { get; set; }
+    public MediaType Type { get; set; }
+    public IFormFile File { get; set; } = null!;
+    public IFormFile? Thumbnail { get; set; }
+}
